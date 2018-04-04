c5eec01a9c66d269765b61b91acc13b37497f3fd
Erik Ovegard <erik.ovegard@stormgeo.com>
2018-04-04 22:25:22 +0200

    It's alive!

--- RobotController.MicroController/wallewebserver.ino
+++ RobotController.MicroController/wallewebserver.ino
@@ -10,14 +10,14 @@ const char *ssid = "EoE";
 
 ESP8266WebServer server ( 80 );
 
-const int num_servos = 4;
+const int num_servos = 5;
 Servo servos[num_servos];
 
 const int leftMotorPin = 4;
 const int rightMotorPin = 5;
 const int headPin = 2;
-
-const int led = 13;
+const int eyePin1 = 15;
+const int eyePin2 = 13;
 
 void handleServos() {
 	int numArgs = server.args();
@@ -48,7 +48,6 @@ void handleAnalog() {
 }
 
 void handleRoot() {
-	digitalWrite ( led, 1 );
 	char temp[400];
 	int sec = millis() / 1000;
 	int min = sec / 60;
@@ -73,11 +72,9 @@ void handleRoot() {
 		hr, min % 60, sec % 60
 	);
 	server.send ( 200, "text/html", temp );
-	digitalWrite ( led, 0 );
 }
 
 void handleNotFound() {
-	digitalWrite ( led, 1 );
 	String message = "File Not Found\n\n";
 	message += "URI: ";
 	message += server.uri();
@@ -92,12 +89,9 @@ void handleNotFound() {
 	}
 
 	server.send ( 404, "text/plain", message );
-	digitalWrite ( led, 0 );
 }
 
 void setup ( void ) {
-	pinMode ( led, OUTPUT );
-	digitalWrite ( led, 0 );
 	Serial.begin ( 115200 );
 	WiFi.begin ( ssid, password );
 	Serial.println ( "" );
@@ -123,7 +117,6 @@ void setup ( void ) {
 	server.on ( "/", handleRoot );
 	server.on ( "/analog", handleAnalog );
 	server.on("/servos", handleServos);
-	//server.on ( "/test.svg", drawGraph );
 	server.on ( "/inline", []() {
 		server.send ( 200, "text/plain", "this works as well" );
 	} );
@@ -135,6 +128,15 @@ void setup ( void ) {
 	servos[1].attach(5);  
 	servos[2].attach(2);  
 	servos[3].attach(16);
+	servos[4].attach(0);
+	pinMode(eyePin1, OUTPUT);
+	pinMode(eyePin2, OUTPUT);
+
+	servos[3].write(128);
+	servos[4].write(128);
+
+	analogWrite(eyePin1, 255);
+	analogWrite(eyePin2, 128);
 }
 
 void loop ( void ) {
